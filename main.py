af89d422674c89f72bdc17448c0002e11062834e
Antriksh004 <antriksh0407@gmail.com>
2023-10-06 22:02:41 +0530

    deleted unnecessary files

--- main.py
+++ main.py
@@ -1,28 +1,67 @@
 import csv
-# fields = ["AccountNo","PIN","BankBalance"]
+fileName = "accountHolders.csv"
 
-# values = [[1040433,9807,17000],
-#           [7204033,6807,18090],
-#           [9244933,9867,17000],
-#           [3044403,9897,19000],
-#           [6204409,6007,18000],
-#           [9044303,9667,17090]]
-filename = "accountHolders.csv"
-with open(filename,'r') as csvfile:
-    # csvwriter = csv.writer(csvfile)
-    # csvwriter.writerow(fields)
-    # csvwriter.writerows(values)
-    csvReader = csv.reader(csvfile,delimiter=',')
-    # a = int(input("enter the account no. :"))
-    # b = int(input("enter your PIN :"))
-    for row in enumerate(csvReader):
-        # if a == row[1] and b == row[1]:
-        #     print("the user is valid")
-        # else:
-        lst = row[1]
-        # if a == lst[0] and b == lst[1]:
-        #     print("access approved")
-        # else:
-        #     print("denied")
-        print(lst[1])
+
+
+def addingFieldName():
+    with open(fileName, mode ='a', newline="") as csvFile:
+        csvWriter = csv.writer(csvFile,delimiter=',')
+        csvWriter.writerow(["AccountNo","PIN"])
+    
+
+def register():
+    with open(fileName, mode ='a', newline="") as csvFile:
+        csvWriter = csv.writer(csvFile,delimiter=',')
+        accountNo = input("enter your ACCOUNT NO")
+        pin = input("enter your PIN")
         
+        csvWriter.writerow([accountNo,pin])
+        print("registration successful")
+
+
+def checkUser():
+
+    accountNo = input("enter your ACCOUNT NO")
+    pin = input("enter your PIN")
+    
+    with open(fileName, mode='r') as csvFile:
+        csvReader = csv.reader(csvFile, delimiter = ",")
+        for row in csvReader:
+            if row == [accountNo,pin]:
+                print("you are Logged in")
+                return True
+            
+    print("please try again")
+    for i in range(3):
+        checkUser()
+    print("you have attempted to enter the information 3 times")
+                    
+        
+# in case you want to register user just uncomment the line below
+# for i in range(4):
+#     register()
+
+def main():
+    print("WELCOME TO THE BANK OF BADLAPUR")
+    query = int(input("please press 1 to enter the account details ..."))
+    if query == 1:
+        
+        if checkUser() == True:
+            
+            amount = int(input("Availabe denominations are 100 200 500 \n enter the amount to withdraw : "))
+            if(amount%100 == 0 or amount%200 == 0 or amount%500 == 0):
+                print("your transaction amount is ",amount)
+                check1 = input("type yes to proceed for transation:")
+                if check1 == "yes" or check1 == "YES":
+                    print("Transation was successful \n Thank you for using this ATM")
+            else:
+                print("the notes of the given money cannot be provided")      
+                
+
+main()           
+        
+    
+
+        
+
+    
